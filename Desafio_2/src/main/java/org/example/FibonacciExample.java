19a1ce1e3beb7c175f8e18c16ce06baeac543a38
Matheus Gabriel Tatangelo <54727159+matheustatangelo@users.noreply.github.com>
2024-03-05 04:21:55 -0300

    Update FibonacciExample.java

--- Desafio_2/src/main/java/org/example/FibonacciExample.java
+++ Desafio_2/src/main/java/org/example/FibonacciExample.java
@@ -2,13 +2,13 @@ package org.example;
 import java.util.Scanner;
 public class FibonacciExample {
     public static void main(String[] args) {
-        // Defina o número limite para gerar a sequência de Fibonacci
+        
         int limiteSuperior = 10000;
 
-        // Gere a sequência de Fibonacci até o limite superior
+
         int[] sequenciaFibonacci = generateFibonacci(limiteSuperior);
 
-        // Solicite um número ao usuário
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número para verificar se está na sequência de Fibonacci: ");
         int numeroInformado = scanner.nextInt();
